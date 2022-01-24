f3033694eb0a46d9a533fb3e0b31d564129183a0
thanhlucvip2 <doanthanhluc91bvh@gmail.com>
2022-01-24 21:57:49 +0700

    day1

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE thanhlucvip
+-- tạo database
+
+GO -- chờ lệnh trên thực thi xong thì tiếp tục
+
+USE thahlucvip -- sử dụng database thanhlucvip
+
+CREATE TABLE HOCSINH (
+	name nvarchar(10)
+	age int
+) --tạo table hocsinh
+
+ALTER TABLE HOCSINH add ngaysinh Date -- sửa bảng thêm cột ngày sinh kiểu date
+
+TRUNCATE TABLE HOCSINH -- xóa dữ liệu bảng 
+
+DROP TABLE HOCSINH -- xóa table
+DROP DATABASE thanhlucvip --xóa databse
